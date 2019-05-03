534834888d3ed4af090769f93f9222499492940b
Travis Toal <travisty12@gmail.com>
2019-05-03 14:02:03 -0700

    add 404 path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import Error404 from './Error404';
 function App(){
   return (
     <div>
-      <Header/>
       <Switch>
         <Route exact path='/' component={Landing} />
         <Route path='/room' component={Room} />

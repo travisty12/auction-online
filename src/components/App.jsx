ce4ac4f32af47f4dc0e144e1ff68077c4d9ad92f
Travis Toal <travisty12@gmail.com>
2019-05-03 15:12:23 -0700

    add styling for landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,51 @@ import Room from './Room';
 import Error404 from './Error404';
 
 function App(){
+  let marginWidth;
+  let marginTop;
+  function updateMargin() {
+    if (window.innerWidth > 1000) {
+      marginWidth = `calc((100vw - 900px) / 2)`;
+    } else {
+      marginWidth = `10vw`;
+    }
+  }
+  updateMargin();
+  window.addEventListener('resize', function() {
+      console.log(window.innerWidth);
+      updateMargin();
+    }, true);
+
   return (
-    <div>
+    <div className="mainContainer">
+      <style jsx>{`
+        .mainContainer {
+          background-color: yellow;
+          width: 80vw;
+          max-width: 900px;
+          height: 90vh;
+          margin-top: 5vh;
+          margin-left: ${marginWidth};
+          padding: 0 10% 0 10%;
+        }
+        `}</style>
+      <style global jsx>{`
+        * {
+          padding: 0;
+          margin: 0;
+        }
+        body {
+          background-color: red;
+          display: flex;
+          justify-conent: center;
+        }
+        `}</style>
       <Switch>
         <Route exact path='/' component={Landing} />
         <Route path='/room' component={Room} />
         <Route path='/setup' component={Setup} />
         <Route component={Error404} />
       </Switch>
-      <style global jsx>{`
-        * {
-          padding: 0;
-          margin: 0;
-        }
-      `}</style>
 
     </div>
   );
